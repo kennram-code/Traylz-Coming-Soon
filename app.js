51cb41940d82cf9ffa84865f31a88ec67351ccb1
kennram-code <63313596+kennram-code@users.noreply.github.com>
2022-05-18 19:15:34 +0100

    update privacy, cookies policies and banner

--- app.js
+++ app.js
@@ -1,3 +1,48 @@
+const cookieStorage = {
+    getItem: (item) => {
+        const cookies = document.cookie
+            .split(';')
+            .map(cookie => cookie.split('='))
+            .reduce((acc, [key, value]) => ({ ...acc, [key.trim]: value}), {})
+        return cookies[item]
+    },
+    setItem: (item, value) => {
+        document.cookie = `${item}=${value}`
+    }
+}
+
+const storageType = cookieStorage;
+const consentPropertyName = 'Traylz_consent';
+const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
+const saveToStorage = () => storageType.setItem(consentPropertyName, true);
+
+window.onload = () => {
+
+    const acceptFn = event => {
+        saveToStorage(storageType);
+        cookieContainer.classList.add('hidden')
+    }
+
+    const cookieContainer =  document.getElementById('cookie-container')
+    const cookieButton = document.getElementById('cookie-btn')
+
+    
+
+    cookieButton.addEventListener('click', acceptFn)
+
+    if (shouldShowPopup(storageType)) {
+        setTimeout(() => {
+            cookieContainer.classList.remove('hidden')
+        }, 2000)
+        
+        }
+    }
+
+
+
+
+
+
 const menuToggle = document.querySelector('.toggle')
         const showcase = document.querySelector('.showcase')
 
@@ -5,3 +50,6 @@ const menuToggle = document.querySelector('.toggle')
             menuToggle.classList.toggle('active')
             showcase.classList.toggle('active')
         })
+
+
+
